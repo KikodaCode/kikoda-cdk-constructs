ff2e0bdbd2e06f403b09f69893a4cb771f92641f
Timothy Harris <timothy.harris@kikoda.com>
2022-04-25 14:45:09 +0000

    chore: project created with projen

---
+++ .projenrc.js
@@ -0,0 +1,15 @@
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Timothy Harris',
+  authorAddress: 'timothy.harris@kikoda.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  name: 'kikoda-cdk-constructs',
+  repositoryUrl: 'https://github.com/KikodaCode/kikoda-cdk-constructs.git',
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
