9c4402b2c40c9db3577b0269ea9cfb44a1bf99cd
Timothy Harris <timothy.harris@kikoda.com>
2022-04-29 13:43:26 +0000

    Added devcontainer config

--- .projenrc.ts
+++ .projenrc.ts
@@ -15,6 +15,6 @@ const project = new awscdk.AwsCdkConstructLibrary({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  packageName: "@kikoda/cdk-constructs",
 });
 project.synth();
