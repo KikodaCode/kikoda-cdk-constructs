79dfdb93d0178599e94ab1c7ce21037b4a58888c
Ben <benjamin.hybart@kikoda.com>
2022-05-04 04:39:02 +0000

    Added website tests

--- test/Website.test.ts
+++ test/Website.test.ts
@@ -1,6 +1,7 @@
 import { Stack } from "aws-cdk-lib";
 import { Match, Template } from "aws-cdk-lib/assertions";
 import { HostedZoneProviderProps } from "aws-cdk-lib/aws-route53";
+import { CorsRule, HttpMethods } from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { Website, WebsiteProps } from "../src";
 
@@ -32,12 +33,26 @@ describe("Given a Website", () => {
   }
   const baseDomain = "baseDomain";
   const subDomain = "subDomain";
+  const corsRules = [
+    {
+      allowedHeaders: ["*"],
+      allowedMethods: [HttpMethods.GET],
+      allowedOrigins: ["*"],
+      exposedHeaders: [
+        "x-amz-server-side-encryption",
+        "x-amz-request-id",
+        "x-amz-id-2",
+      ],
+      maxAge: 3000,
+    },
+  ];
 
   const webStack = new WebStack({
     stage: "test",
     appDir: "test",
     subdomain: subDomain,
     baseDomain: baseDomain,
+    enableCors: true,
   });
   const template = Template.fromStack(webStack);
   test(`CF Distribution Alias contains ${baseDomain} and ${subDomain}`, () => {
@@ -46,6 +61,22 @@ describe("Given a Website", () => {
         Aliases: [
           Match.stringLikeRegexp(`(?=.*${subDomain})(?=.*${baseDomain}).*`),
         ],
+      },
+    });
+  });
+
+  test("CF Distribution should have CorsConfiguration", () => {
+    template.hasResourceProperties("AWS::S3::Bucket", {
+      CorsConfiguration: {
+        CorsRules: corsRules.map((rule: CorsRule) => {
+          return {
+            AllowedHeaders: rule.allowedHeaders,
+            AllowedMethods: rule.allowedMethods,
+            AllowedOrigins: rule.allowedOrigins,
+            ExposedHeaders: rule.exposedHeaders,
+            MaxAge: rule.maxAge,
+          };
+        }),
       },
     });
   });
