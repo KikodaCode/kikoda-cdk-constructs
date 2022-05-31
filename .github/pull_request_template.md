476b0553f83697b965646d5debd17f8e1f547759
nathantcz <nathan.cazell@kikoda.com>
2022-05-31 15:33:39 +0000

    chore: upgrade main; use kikoda oss template; use gh app creds

--- .github/pull_request_template.md
+++ .github/pull_request_template.md
@@ -10,15 +10,15 @@ _Anything else we should know when reviewing?_
 
 What types of changes does your code introduce? _Chedk all the boxes that apply_
 
-- [ ] Bugfix(non - breaking change which fixes an issue)
-- [ ] New feature or Enhancement(non - breaking change which adds functionality)
-- [ ] Breaking change(fix or feature that would cause existing functionality to not work as expected)
-- [ ] Chore(documentation, refactoring, dependency upgrade, etc.)
+- [ ] Bugfix (non - breaking change which fixes an issue)
+- [ ] New feature or Enhancement (non - breaking change which adds functionality)
+- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
+- [ ] Chore (documentation, refactoring, dependency upgrade, etc.)
 
 ### Chores
 
 _Check all the boxes that apply_
 
-- [ ] This PR includes breaking changes which are documented in the description and associated commit messages(see the[Contributing Guide](../ CONTRIBUTING.md) for more information on how this should be done)
-- [ ] This PR impacts documentation, and it has been updated(or an issue has been created and linked)
+- [ ] This PR includes breaking changes which are documented in the description and associated commit messages (see the [Contributing Guide](../CONTRIBUTING.md) for more information on how this should be done)
+- [ ] This PR impacts documentation, and it has been updated (or an issue has been created and linked)
 - [ ] This PR's changes are covered by the automated tests
--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,6 @@
-import { TextFile } from 'projen';
+import { KikodaOpenSourceProject } from '@kikoda/projen-templates';
 import { AwsCdkConstructLibrary } from 'projen/lib/awscdk';
+import { GithubCredentials } from 'projen/lib/github';
 import { ArrowParens, EndOfLine, TrailingComma } from 'projen/lib/javascript';
 
 const project = new AwsCdkConstructLibrary({
@@ -35,46 +36,22 @@ const project = new AwsCdkConstructLibrary({
   tsconfig: { compilerOptions: { esModuleInterop: true } },
 
   // deps: [],                /* Runtime dependencies of this module. */
-  devDeps: ['@types/md5', '@types/uuid', '@types/lodash'] /* Build dependencies for this module. */,
+  devDeps: [
+    '@types/md5',
+    '@types/uuid',
+    '@types/lodash',
+    '@kikoda/projen-templates',
+  ] /* Build dependencies for this module. */,
   packageName: '@kikoda/cdk-constructs',
   gitignore: ['spa_local_build_artifact'],
-  pullRequestTemplateContents: [
-    `## Proposed changes`,
-    '',
-    `_Describe the big picture of your changes here to communicate to the maintainers why we should accept this pull request.If it fixes a bug or resolves a feature request, be sure to link to that issue._`,
-    '',
-    `### Commentary`,
-    '',
-    `_Anything else we should know when reviewing?_`,
-    '',
-    `### Types of Changes`,
-    '',
-    `What types of changes does your code introduce? _Chedk all the boxes that apply_`,
-    '',
-    `- [ ] Bugfix(non - breaking change which fixes an issue)`,
-    `- [ ] New feature or Enhancement(non - breaking change which adds functionality)`,
-    `- [ ] Breaking change(fix or feature that would cause existing functionality to not work as expected)`,
-    `- [ ] Chore(documentation, refactoring, dependency upgrade, etc.)`,
-    '',
-    `### Chores`,
-    '',
-    `_Check all the boxes that apply_`,
-    '',
-    `- [ ] This PR includes breaking changes which are documented in the description and associated commit messages(see the[Contributing Guide](../ CONTRIBUTING.md) for more information on how this should be done)`,
-    `- [ ] This PR impacts documentation, and it has been updated(or an issue has been created and linked)`,
-    `- [ ] This PR's changes are covered by the automated tests`,
-  ],
+  githubOptions: {
+    projenCredentials: GithubCredentials.fromApp(),
+  },
+  pullRequestTemplate: false,
 });
 
-// Generate a NOTICE file for license
-new TextFile(project, 'NOTICE', {
-  marker: false,
-  lines: [
-    'Kikoda CDK Constructs Library',
-    'Copyright 2022-2022 Kikoda, LLC',
-    '',
-    'This product includes software developed at Kikoda (https://www.kikoda.com)',
-  ],
+new KikodaOpenSourceProject(project, {
+  title: 'Kikoda CDK Constructs Library',
 });
 
 project.synth();
