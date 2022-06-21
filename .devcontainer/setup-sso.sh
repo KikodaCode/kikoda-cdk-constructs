3e3d46338c37d654eebb867467f1dd3674873cfe
Timothy Harris <timothy.harris@kikoda.com>
2022-06-21 20:19:59 +0000

    fix: updated resource statement of assume role policy statement of asset publish defaults when code artifacts repository access is required.

---
+++ .devcontainer/setup-sso.sh
@@ -0,0 +1,23 @@
+if [ ! -d "/home/node/.aws" ]
+then
+    mkdir /home/node/.aws
+fi
+
+subdomain="kikoda"
+account="703400946075"
+roleName="SystemAdministrator"
+
+echo "[default]" > /home/node/.aws/config
+echo "sso_start_url = https://$subdomain.awsapps.com/start" >> /home/node/.aws/config
+echo "sso_region = us-east-1" >> /home/node/.aws/config
+echo "sso_account_id = $account" >> /home/node/.aws/config
+echo "sso_role_name = $roleName" >> /home/node/.aws/config
+echo "region = us-east-1" >> /home/node/.aws/config
+echo "output = json" >> /home/node/.aws/config
+echo "[profile nica]" >> /home/node/.aws/config
+echo "sso_start_url = https://$subdomain.awsapps.com/start" >> /home/node/.aws/config
+echo "sso_region = us-east-1" >> /home/node/.aws/config
+echo "sso_account_id = $account" >> /home/node/.aws/config
+echo "sso_role_name = $roleName" >> /home/node/.aws/config
+echo "region = us-east-1" >> /home/node/.aws/config
+echo "output = json" >> /home/node/.aws/config
